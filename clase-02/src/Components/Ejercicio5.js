c14cbab232913895f65a2a63b63bdfc6f76c3233
Tomás Marzzellino <tmarzzellino@dguiaf-gcba.gov.ar>
2022-11-02 17:33:05 -0300

    agrego final de ejercicio6

--- clase-02/src/Components/Ejercicio5.js
+++ clase-02/src/Components/Ejercicio5.js
@@ -1,5 +1,6 @@
 const estiloH1 = {
-    fontSize: '80px'
+    fontSize: '80px',
+    weight: 'bold',
 }
 
 const estiloP1 = {
@@ -7,7 +8,7 @@ const estiloP1 = {
 }
 
 const estiloP2 = {
-    fontSize: '30px',
+    fontSize: '20px',
     color: 'grey'
 }
 
@@ -15,13 +16,13 @@ const Ejercicio5 = () => {
     return (
         <div>
             <h1 style={estiloH1}>
-                Cuatro zapatos con motor electrico para
+                Cuatro zapatos con motor electrico para correr más rápido
             </h1>
             <p style={estiloP1}>
-                P1
+                La tecnología siempre busca dar comodidad a acciones diarias y en todo el mundo se han creado
             </p>
             <p style={estiloP2}>
-                p2
+                28 de Octubre de 2022
             </p>
         </div>
     );
