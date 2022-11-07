bb5030e462981e14933985eb05568567c312d537
Tomás Marzzellino <tmarzzellino@dguiaf-gcba.gov.ar>
2022-11-07 18:53:03 -0300

    Agrego contenido de clase 04 (parcial)

--- clase-04/src/App.js
+++ clase-04/src/App.js
@@ -1,12 +1,17 @@
+//EJERCICIO: Crear caja de login de instagram
+// ME QUEDE EN EL MINUTO 1.19.47 DE LA CLASE EN VIVO
+
 import './App.css';
-import Ejercicio01 from './Components/Ejercicio01';
+import FormularioInicioSesion from './Components/EjemploFormularioInicioSesion';
+// import Ejercicio01 from './Components/Ejercicio01';
 // import EjemploInput01 from './Components/EjemploInput01';
 
 function App() {
   return (
     <div className="App">
       {/* <EjemploInput01 /> */}
-      <Ejercicio01 />
+      {/* <Ejercicio01 /> */}
+      <FormularioInicioSesion />
     </div>
   );
 }
