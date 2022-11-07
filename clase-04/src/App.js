d566481e002507968331b2ea45c1e63e17a77697
SrTomson <74432095+SrTomson@users.noreply.github.com>
2022-11-07 17:38:33 -0300

    agrego archivos de clase-04 en vivo (parcial)

---
+++ clase-04/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Ejercicio01 from './Components/Ejercicio01';
+// import EjemploInput01 from './Components/EjemploInput01';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <EjemploInput01 /> */}
+      <Ejercicio01 />
+    </div>
+  );
+}
+
+export default App;
