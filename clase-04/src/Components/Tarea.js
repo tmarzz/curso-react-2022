76509c796f8032ff82787d92132a32a496e631b1
tmarzz <104935013+tmarzz@users.noreply.github.com>
2022-11-09 20:10:01 -0300

    Clase 05 (#2)
    * Liempieza de archivos clase-04
    
    * Clase 05
    
    Co-authored-by: Tomás Marzzellino <tmarzzellino@dguiaf-gcba.gov.ar>

--- clase-04/src/Components/Tarea.js
+++
@@ -1,9 +0,0 @@
-/*
-    Dar estilos al formulario (Facebook o Instagram)
-
-    Crear un componente input que controle que el ingreso sea:
-        -   Una palabra de no mas de 5 caracteres
-        -   Con dos numeros
-        -   Que los numeros no esten consecutivos
-        -   No puede comenzar con numeros. El primer caracter no puede ser un numero.
-*/
