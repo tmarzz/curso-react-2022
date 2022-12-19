449792cf8df33fffba4aa2980ce9424fa2debeb8
tmarzz <marzzellinot@gmail.com>
2022-12-19 18:02:25 -0300

    Clase 12 (parcial)

---
+++ clase-12/src/App.js
@@ -0,0 +1,18 @@
+// 1.18.52
+
+import './App.css';
+import Contador from './Components/EjemploHookUseStateEstadoPrevio';
+// import PostItem from './Components/PostItem';
+import Tarjetero from './Components/Tarjetero';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PostItem /> */}
+      {/* <Tarjetero /> */}
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
