fdb75f31e5ab0e24ac96c93c8d591acec47efb1a
tmarzz <104935013+tmarzz@users.noreply.github.com>
2022-12-21 15:05:22 -0300

    Clase 12 (#10)
    * Limpieza archivos
    
    * Clase 12 (parcial)
    
    * Clase 12 (completo)
    
    * Clase 12 (parcial)
    
    * Clase 12 (completo)
    
    * Limpieza

---
+++ clase-12/src/App.js
@@ -0,0 +1,21 @@
+// 1.18.52
+
+import './App.css';
+// import Contador from './Components/EjemploHookUseStateEstadoPrevio';
+// import EjemploUseStateObjeto from './Components/EjemploHookUseStateObjeto';
+// import PostItem from './Components/PostItem';
+import Tarjetero from './Components/Tarjetero';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PostItem /> */}
+      <Tarjetero />
+      {/* <Contador /> */}
+      {/* <EjemploUseStateObjeto /> */}
+
+    </div>
+  );
+}
+
+export default App;
